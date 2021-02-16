a9e834ac4688350e470ec55ac7b5ccddcece940d
fanny <fanny.wastelin@hotmail.com>
2021-02-16 12:59:12 +0100

    ajout modal sur projet hacka/ debugger pour fermer

--- pages/home.js
+++ pages/home.js
@@ -19,17 +19,24 @@ export const home = () => {
           <section class="ligne1">
             <div class="projet1"><div class="img-less"><span class="texte">Site "Less is Belge": projet de groupe réalisé à Interface3.<button class="voir-less">En voir plus</button></span></div></div>
             <div class="ai"><div class="img-ai"><span class="texte">Carte postale déssinée en Illustrator.</span></div></div>
-            <div class="hackaton"><div class="img-haka"><span class="texte">Projet Hackathon 2020 pour promouvoir l'informatique au féminin.<button class="voir-hacka">En voir plus</button></span></div></div>
+            <div class="hackaton"><div class="img-haka"><span class="texte">Projet Hackathon 2020 pour promouvoir l'informatique au féminin.<a href="#modal-hacka" class="js-modal">modal</a><button class="voir-hacka">En voir plus</button></span></div></div>
           </section>
           <section class="ligne2">
             <div class="phaser"><div class="img-pha"><span class="texte">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit excepturi placeat sed molestiae dolorem</span></div></div>
             <div class="dei"><div class="img-dei">
-              <span class="texte">Projet de classe réalisé pour "Défense des enfants international Belgique" <button class="modal-dei">modal</button><button class="voir-dei"><a href="https://www.dei-belgique.be/expo-bridge/">Découvrir</a></button></span></div></div>
+              <span class="texte">Projet de classe réalisé pour "Défense des enfants international Belgique" <button class="voir-dei"><a href="https://www.dei-belgique.be/expo-bridge/">Découvrir</a></button></span></div></div>
             <div class="animate"></div>
           </section>
           <div class="container-tache3"><div class="tache3"></div></div>
         </section>
       </article>
+      <aside id="modal-hacka" class="modal" style="display:none;">
+        <div class="contenu-modal">
+          <button class="js-modal-close">X</button>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit excepturi placeat sed molestiae dolorem
+          </p>
+        </div>
+      </aside>
 
       <article class="competence">
         <h2><a name="competence">Compétences</a></h2>
@@ -64,4 +71,26 @@ export const home = () => {
     `;
   document.querySelector('main').innerHTML = contentHome;
   $('.voir-less').on('click', () => less());
+
+  /* modal */
+  const modal = null;
+
+  const openModal = function (e) {
+    e.preventDefault();
+    const target = document.querySelector(e.target.getAttribute('href'));
+    target.style.display = null;
+    modal = target;
+    modal.addEventListener('click', closeModal);
+    modal.querySelector('.js-modal-close').addEventListener('click', closeModal);
+  };
+  const closeModal = function (e) {
+    e.preventDefault();
+    modal.style.display = 'none';
+    modal.removeEventListener('click', closeModal);
+    modal.querySelector('.js-modal-close').removeEventListener('click', closeModal);
+    modal = null;
+  };
+  document.querySelectorAll('.js-modal').forEach((a) => {
+    a.addEventListener('click', openModal);
+  });
 };
