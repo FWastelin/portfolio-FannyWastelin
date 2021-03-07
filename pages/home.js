2ce4b7a34ab3be97dffda2bf62c532b48f2ac3cb
fanny <fanny.wastelin@hotmail.com>
2021-03-07 18:53:48 +0100

    ajout texte corecte

--- pages/home.js
+++ pages/home.js
@@ -7,7 +7,7 @@ export const home = () => {
         <article class="apropos">
         <h2><a name="apropos">A propos</a></h2>
         <section>
-          <div class="container-txt"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est cupiditate minima animi quasi laudantium dolorum ab inventore iste fuga repellat aliquid facere corrupti, totam voluptas architecto itaque quo officia nesciunt?</p></div>
+          <div class="container-txt"><p>Styliste de formation, je me suis découverte une passion pour le code. Ma formation de « Front-end developer » en poche, je suis prête à relever de nouveaux défis. Mes points forts ? Le souci du travail bien fait, un esprit d’équipe, des compétences techniques, de la créativité et mon intérêt pour le « beau ».</p></div>
           <div class="container-img"><div class="img-portrait"></div></div>
           <div class="container-tache2"><div class="tache2"></div></div>
         </section>
@@ -16,14 +16,14 @@ export const home = () => {
         <h2><a name="projet">Projets</a></h2>
         <section class="img-projet">
           <section class="ligne1">
-            <div class="projet1"><div class="img-less"><span class="texte">Site "Less is Belge": projet de groupe réalisé à Interface3.</br><div class="d-flex"><a href="#modal-lib" class="js-modal">En voir plus<img src="./images/eyes.pdf" alt="img1"/></a><button class="voir"><a href="https://less-is-belge.netlify.app/">Découvrir</a></button></div></span></div></div>
-            <div class="ai"><div class="img-ai"><span class="texte">Carte postale déssinée en Illustrator.</span></div></div>
-            <div class="hackaton"><div class="img-haka"><span class="texte">Projet Hackathon 2020 pour promouvoir l'informatique au féminin.</br><div class="d-flex"><a href="#modal-hacka" class="js-modal">En voir plus</a><button class="voir"><a href=" https://logici-elles.if3.dev/">Découvrir</a></button></div></span></div></div>
+            <div class="projet1"><div class="img-less"><span class="texte">Projet de groupe réalisé à 3 lors de ma formation. Objectif : promouvoir la mode éco-responsable belge.</br>Technologies utilisées : Javascript, HTML, SCSS, Webpack. </br><div class="d-flex"><a href="#modal-lib" class="js-modal">En voir Plus</a><button class="voir"><a href="https://less-is-belge.netlify.app/">Découvrir</a></button></div></span></div></div>
+            <div class="ai"><div class="img-ai"><span class="texte">Carte postale dessinée avec Illustrator.</span></div></div>
+            <div class="hackaton"><div class="img-haka"><span class="texte">Projet réalisé lors du Hackathon 2020 de mon centre de formation. Objectif : promouvoir l’informatique au féminin.</br>Technologies utilisées : Javascript, HTML, SCSS, Parcel.</br><div class="d-flex"><a href="#modal-hacka" class="js-modal">En voir plus</a><button class="voir"><a href=" https://logici-elles.if3.dev/">Découvrir</a></button></div></span></div></div>
           </section>
           <section class="ligne2">
-            <div class="phaser"><div class="img-pha"><span class="texte">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit excepturi placeat sed molestiae dolorem</span></div></div>
+            <div class="phaser"><div class="img-pha"><span class="texte">R-Types games réalisés durant ma formation à Interface3 (2 jours).</br>Technologie utilisée :Phaser</span></div></div>
             <div class="dei"><div class="img-dei">
-              <span class="texte">Projet de classe réalisé pour "Défense des enfants international Belgique" <button class="voir-dei"><a href="https://www.dei-belgique.be/expo-bridge/">Découvrir</a></button></span></div></div>
+              <span class="texte">Site web créé avec l’ensemble de la classe. Client : « Défense des enfants International (DEI) Belgique. Technologies utilisées : React, HTML, SCSS.</br><button class="voir-dei"><a href="https://www.dei-belgique.be/expo-bridge/">Découvrir</a></button></span></div></div>
             <div class="animate"></div>
           </section>
           <div class="container-tache3"><div class="tache3"></div></div>
@@ -59,11 +59,10 @@ export const home = () => {
         <h2><a name="competence">Compétences</a></h2>
         <section class="sect-competence">
           <div class="technique">
-            <h3>Technique</h3>
+            <h3>Techniques</h3>
             <ul>
               <li>Html5/ Css3/ Sass</li>
-              <li>UX/UI</li>
-              <li>JavaScript/ jQuery</li>
+              <li>JavaScript/ jQuery/ React</li>
               <li>Git/ Github</li>
               <li>PHP/ MySql</li>
               <li>Node.js/noSql</li>
